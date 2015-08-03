afcaefedf9d94d0cd6e7d2c69dec1506520dba43
Michael Woolnough <michael.woolnough@gmail.com>
2015-08-03 12:41:36 +0100

    Start of upload handling

--- web.go
+++ web.go
@@ -3,24 +3,44 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
-	"net/rpc/jsonrpc"
 	"os"
 	"os/signal"
 
+	"github.com/MJKWoolnough/byteio"
 	"golang.org/x/net/websocket"
 )
 
-func rpcHandler(conn *websocket.Conn) {
-	jsonrpc.ServeConn(conn)
+func socketHandler(conn *websocket.Conn) {
+	r := byteio.LittleEndianReader{conn}
+	length, _, err := r.ReadInt64()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	f, err := ioutil.TempFile("", "mineWebGen")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer os.Remove(f.Name())
+	_, err = io.Copy(f, io.LimitReader(conn, int64(length)))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	f.Seek(0, 0)
+	io.Copy(os.Stdout, f)
 }
 
 var port = flag.Uint("-p", 8080, "server port")
 
 func main() {
-	http.Handle("/rpc", websocket.Handler(rpcHandler))
+	http.Handle("/socket", websocket.Handler(socketHandler))
 	http.Handle("/", http.FileServer(dir))
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
