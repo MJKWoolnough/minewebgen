ea4954b39abe65f3f16891f3075a2eb4d51fc7e4
Michael Woolnough <michael.woolnough@gmail.com>
2015-08-04 17:44:23 +0100

    Ready to start processing image

--- web.go
+++ web.go
@@ -12,38 +12,87 @@ import (
 	"os/signal"
 
 	"github.com/MJKWoolnough/byteio"
+	"github.com/MJKWoolnough/ora"
 	"golang.org/x/net/websocket"
 )
 
-func writeError(w io.Writer, err error) {
+func writeError(w *byteio.LittleEndianWriter, err error) {
 	ew := byteio.LittleEndianWriter{Writer: w}
 	ew.WriteUint8(0)
 	errStr := []byte(err.Error())
 	ew.WriteInt64(int64(len(errStr)))
 	ew.Write(errStr)
-	fmt.Println("write error", err)
+	fmt.Println("error:", err)
 }
 
 func socketHandler(conn *websocket.Conn) {
+	conn.PayloadType = websocket.BinaryFrame
 	r := byteio.LittleEndianReader{conn}
+	w := byteio.LittleEndianWriter{Writer: conn}
 	length, _, err := r.ReadInt64()
 	if err != nil {
-		writeError(conn, err)
+		writeError(&w, err)
 		return
 	}
 	f, err := ioutil.TempFile("", "mineWebGen")
 	if err != nil {
-		writeError(conn, err)
+		writeError(&w, err)
 		return
 	}
 	defer os.Remove(f.Name())
-	_, err = io.Copy(f, io.LimitReader(conn, int64(length)))
+	defer f.Close()
+	n, err := io.Copy(f, io.LimitReader(conn, int64(length)))
 	if err != nil {
-		writeError(conn, err)
+		writeError(&w, err)
 		return
 	}
-	conn.Write([]byte{1})
+	if n != length {
+		writeError(&w, io.EOF)
+		return
+	}
 	f.Seek(0, 0)
+	o, err := ora.Open(f, length)
+	if err != nil {
+		writeError(&w, err)
+		return
+	}
+	terrain := o.Layer("terrain")
+	if terrain == nil {
+		writeError(&w, layerError{"terrain"})
+		return
+	}
+	heightMap := o.Layer("height")
+	if heightMap == nil {
+		writeError(&w, layerError{"height"})
+		return
+	}
+	_, err = w.WriteUint8(1)
+	if err != nil {
+		fmt.Println(err)
+		writeError(&w, err)
+		return
+	}
+	b := o.Bounds()
+	_, err = w.WriteInt64(int64(b.Max.X))
+	if err != nil {
+		fmt.Println(err)
+		writeError(&w, err)
+		return
+	}
+	_, err = w.WriteInt64(int64(b.Max.Y))
+	if err != nil {
+		fmt.Println(err)
+		writeError(&w, err)
+		return
+	}
+}
+
+type layerError struct {
+	name string
+}
+
+func (l layerError) Error() string {
+	return "missing layer: " + l.name
 }
 
 var port = flag.Uint("-p", 8080, "server port")
