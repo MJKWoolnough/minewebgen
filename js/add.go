c078e7426d331ee3c8fc9d916798ac651941dec4
Michael Woolnough <michael.woolnough@gmail.com>
2015-08-08 15:12:42 +0100

    Added beginning of Add selecting

---
+++ js/add.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/MJKWoolnough/gopherjs/xjs"
+	"honnef.co/go/js/dom"
+)
+
+func add(c dom.Element) {
+	u := xjs.CreateElement("input")
+	u.SetAttribute("value", "Upload")
+	u.SetAttribute("type", "button")
+	u.AddEventListener("click", false, func(dom.Event) { upload(c) })
+	d := xjs.CreateElement("input")
+	d.SetAttribute("value", "Download")
+	d.SetAttribute("type", "button")
+	d.AddEventListener("click", false, func(dom.Event) { download(c) })
+	g := xjs.CreateElement("input")
+	g.SetAttribute("value", "Generate")
+	g.SetAttribute("type", "button")
+	g.AddEventListener("click", false, func(dom.Event) { generate(c) })
+	c.AppendChild(u)
+	c.AppendChild(d)
+	c.AppendChild(g)
+}
---
+++ js/upload.go
@@ -0,0 +1,11 @@
+package main
+
+import "honnef.co/go/js/dom"
+
+func upload(c dom.Element) {
+
+}
+
+func download(c dom.Element) {
+
+}
