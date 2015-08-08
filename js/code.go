1407fd390cd09e72c199d1fe26a64f608f4a4f34
Michael Woolnough <michael.woolnough@gmail.com>
2015-08-08 13:14:13 +0100

    Started RPC client

--- js/code.go
+++ js/code.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+	"net/rpc"
+	"net/rpc/jsonrpc"
+
 	"github.com/MJKWoolnough/gopherjs/tabs"
 	"github.com/MJKWoolnough/gopherjs/xjs"
+	"github.com/gopherjs/gopherjs/js"
+	"github.com/gopherjs/websocket"
 
 	"honnef.co/go/js/dom"
 )
 
+var jrpc *rpc.Client
+
 func main() {
 	dom.GetWindow().AddEventListener("load", false, func(e dom.Event) {
-		body := dom.GetWindow().Document().(dom.HTMLDocument).Body()
-		xjs.RemoveChildren(body)
-		body.AppendChild(xjs.SetInnerText(xjs.CreateElement("h1"), "Ferrumwood Server"))
-		body.AppendChild(tabs.MakeTabs([]tabs.Tab{
-			{"Maps", maps},
-			{"Upload", upload},
-		}))
+		go func() {
+			conn, err := websocket.Dial("ws://" + js.Global.Get("location").Get("host").String() + "/rpc")
+			if err != nil {
+				dom.GetWindow().Alert("Error connection to RPC server: " + err.Error())
+				return
+			}
+			jrpc = jsonrpc.NewClient(conn)
+			body := dom.GetWindow().Document().(dom.HTMLDocument).Body()
+			xjs.RemoveChildren(body)
+			body.AppendChild(xjs.SetInnerText(xjs.CreateElement("h1"), "Server"))
+			body.AppendChild(tabs.MakeTabs([]tabs.Tab{
+				{"Maps", maps},
+				{"Add", add},
+			}))
+		}()
 	})
 }
