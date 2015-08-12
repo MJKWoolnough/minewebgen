82a00b0733053c48f9cdd40c46bd036528214a5a
Michael Woolnough <michael.woolnough@gmail.com>
2015-08-12 15:46:52 +0100

    Added locking to config and made server list a map

--- config.go
+++ config.go
@@ -3,17 +3,20 @@ package main
 import (
 	"encoding/json"
 	"os"
+	"sync"
 )
 
 type Server struct {
+	ID         uint32
 	Name, Path string
 }
 
 type Config struct {
+	mu         sync.RWMutex
 	filename   string
 	ServerName string
 	Port       uint16
-	Servers    []Server
+	Servers    map[uint32]Server
 	selected   int
 }
 
@@ -26,7 +29,7 @@ func loadConfig(filename string) (*Config, error) {
 		filename:   filename,
 		ServerName: "Minecraft",
 		Port:       8080,
-		Servers:    make([]Server, 0),
+		Servers:    make(map[uint32]Server, 0),
 		selected:   -1,
 	}
 	err = json.NewDecoder(f).Decode(c)
@@ -37,6 +40,8 @@ func loadConfig(filename string) (*Config, error) {
 }
 
 func (c *Config) save() error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	f, err := os.Create(c.filename)
 	if err != nil {
 		return err
@@ -46,11 +51,18 @@ func (c *Config) save() error {
 }
 
 func (c *Config) Name(_ struct{}, serverName *string) error {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 	*serverName = c.ServerName
 	return nil
 }
 
 func (c *Config) List(_ struct{}, list *[]Server) error {
-	*list = c.Servers
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	*list = make([]Server, 0, len(c.Servers))
+	for _, s := range c.Servers {
+		*list = append(*list, s)
+	}
 	return nil
 }
