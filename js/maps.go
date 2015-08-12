1c8137601ee351bb37868e29e4b1d98e7fb50cc7
Michael Woolnough <michael.woolnough@gmail.com>
2015-08-12 09:29:09 +0100

    Initial map list processing

--- js/maps.go
+++ js/maps.go
@@ -5,11 +5,17 @@ import (
 	"honnef.co/go/js/dom"
 )
 
-var mapsDiv = xjs.CreateElement("div")
-
 func maps(c dom.Element) {
-	if !mapsDiv.HasChildNodes() {
-		xjs.SetInnerText(mapsDiv, "MAPS")
+	mapsDiv := xjs.CreateElement("div")
+	defer c.AppendChild(mapsDiv)
+	list, err := ServerList()
+	if err != nil {
+		xjs.SetInnerText(mapsDiv, err.Error())
+		return
 	}
-	c.AppendChild(mapsDiv)
+	for _, s := range list {
+		sd := xjs.CreateElement("div")
+		xjs.SetInnerText(sd, s.Name)
+		mapsDiv.AppendChild(sd)
+	}
 }
