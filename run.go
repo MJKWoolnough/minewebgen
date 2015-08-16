f237f20d6ebd8ae38301d4aca22905fe78231311
Michael Woolnough <michael.woolnough@gmail.com>
2015-08-16 18:23:04 +0100

    Fleshed out server controls

--- run.go
+++ run.go
@@ -1,11 +1,78 @@
 package main
 
-var serverRunning = -1
+import (
+	"errors"
+	"io"
+	"os/exec"
+	"path"
 
-func (c *Config) stopServer(id int) error {
-	return nil
+	"github.com/armon/circbuf"
+)
+
+type running struct {
+	id    int
+	cmd   *exec.Cmd
+	cb    *circbuf.Buffer
+	stdin io.Writer
+	w     io.Writer
 }
+
+func (r *running) Write(p []byte) (int, error) {
+	if r.id < 0 {
+		return 0, ErrServerNotRunning
+	}
+	return r.w.Write(p)
+}
+
+var serverRunning = &running{id: -1}
+
+func stopServer() error {
+	if serverRunning.ID < 0 {
+		return ErrServerNotRunning
+	}
+	serverRunning.stdin.Write(stopCmd)
+	err := serverRunning.cmd.Wait()
+	serverRunning = &running{id: -1}
+	return err
+}
+
+func saveServer() error {
+	if serverRunning.ID < 0 {
+		return ErrServerNotRunning
+	}
+	_, err := serverRunning.stdin.Write(saveCmd)
+	return err
+}
+
+var (
+	saveCmd = []byte{'\r', '\n', 's', 'a', 'v', 'e', 'a', 'l', 'l', ' ', '\r', '\n'}
+	stopCmd = []byte{'\r', '\n', 's', 't', 'o', 'p', ' ', '\r', '\n'}
+)
 
 func (c *Config) startServer(id int) error {
-	return nil
+	if serverRunning.id < 0 {
+		return ErrServerRunning
+	}
+	r := &running{id: id}
+	s := c.Servers[id]
+	d, e := path.Split(s.Path)
+	r.cmd = exec.Command(e, s.Args...)
+	r.cmd.Dir = d
+	r.cb = circbuf.NewBuffer(1024 * 1024)
+	stdin, err := r.cmd.StdinPipe()
+	if err != nil {
+		return nil
+	}
+	r.stdin = stdin
+	r.w = io.MultiWriter(stdin, r.cb)
+	r.cmd.Stdout = r.cb
+	r.cmd.Stderr = r.cb
+	serverRunning = r
+	return serverRunning.cmd.Start()
 }
+
+// Errors
+var (
+	ErrServerRunning    = errors.New("server already running")
+	ErrServerNotRunning = errors.New("server not running")
+)
