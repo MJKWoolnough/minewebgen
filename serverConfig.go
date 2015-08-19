b7e9c630e92078c9e7f63ba78c7a9718eedcf455
Michael Woolnough <michael.woolnough@gmail.com>
2015-08-19 10:25:12 +0100

    Added simple server.properties reader/writer

---
+++ serverConfig.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"strings"
+)
+
+func ReadConfig(r io.Reader) (map[string]string, error) {
+	br := bufio.NewReader(r)
+	data := make(map[string]string)
+	for {
+		l, err := br.ReadBytes('\n')
+		if err != nil {
+			if err == io.EOF {
+				return data, nil
+			}
+			return nil, errr
+		}
+		if l[0] == '#' {
+			continue
+		}
+		if l[len(l)-1] == '\r' {
+			l = l[:len(l)-1]
+		}
+		parts := strings.SplitN(l, "=", 2)
+		if len(parts) != 2 {
+			continue
+		}
+		data[parts[0]] = parts[1]
+	}
+}
+
+func SaveConfig(w io.Writer, c map[string]string) error {
+	toWrite := make([]byte, 0, 1024)
+	for k, v := range c {
+		toWrite = toWrite[:0]
+		toWrite = append(toWrite, k...)
+		toWrite = append(toWrite, '=')
+		toWrite = append(toWrite, b...)
+		toWrite = append(toWrite, '\n')
+		_, err := w.Write(toWrite)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
