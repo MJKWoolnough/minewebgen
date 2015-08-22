e0b0d9b905ed4b1f56e153c8fb23ced20e171d2a
Michael Woolnough <michael.woolnough@gmail.com>
2015-08-22 10:56:17 +0100

    Implemented unzip

--- utils.go
+++ utils.go
@@ -3,6 +3,9 @@ package main
 import (
 	"archive/zip"
 	"fmt"
+	"io"
+	"os"
+	"path"
 
 	"github.com/MJKWoolnough/byteio"
 )
@@ -16,5 +19,30 @@ func writeError(w *byteio.StickyWriter, err error) {
 }
 
 func unzip(zr *zip.Reader, dest string) error {
+	for _, f := range zr.File {
+		name := path.Join(dest, f.Name)
+		if f.FileInfo().IsDir() {
+			err := os.MkdirAll(name, f.Mode())
+			if err != nil {
+				return err
+			}
+			continue
+		}
+		inf, err := f.Open()
+		if err != nil {
+			return err
+		}
+		of, err := os.OpenFile(name, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			inf.Close()
+			return err
+		}
+		_, err = io.Copy(of, inf)
+		inf.Close()
+		of.Close()
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
