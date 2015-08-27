461bcf056c274b9a1d639be2175ba922e1961f43
Michael Woolnough <michael.woolnough@gmail.com>
2015-08-27 11:05:28 +0100

    Moved uploadFile into utils

--- js/util.go
+++ js/util.go
@@ -7,6 +7,20 @@ import (
 	"github.com/MJKWoolnough/byteio"
 )
 
+type readLener interface {
+	io.Reader
+	Len() int
+}
+
+func uploadFile(id uint8, file readLener, w byteio.StickyWriter) {
+	w.WriteUint8(id)
+	w.WriteInt64(int64(file.Len()))
+	if w.Err == nil {
+		_, err := io.Copy(w.Writer, file)
+		w.Err = err
+	}
+}
+
 func readError(r byteio.StickyReader) error {
 	length := r.ReadUint16()
 	if r.Err != nil {
