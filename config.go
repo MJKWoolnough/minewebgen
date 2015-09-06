4bff9f1440539d7d48bb487baf18a7fccbfd60b4
Michael Woolnough <michael.woolnough@gmail.com>
2015-09-06 17:41:18 +0100

    Seperated RPC methods into own file

--- config.go
+++ config.go
@@ -103,47 +103,6 @@ func (c *Config) save() error {
 	return json.NewEncoder(f).Encode(c)
 }
 
-func (c *Config) Name(_ struct{}, serverName *string) error {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-	*serverName = c.ServerName
-	return nil
-}
-
-func (c *Config) List(_ struct{}, list *[]Server) error {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-	*list = make([]Server, 0, len(c.Servers))
-	for _, s := range c.Servers {
-		*list = append(*list, s)
-	}
-	return nil
-}
-
-func (c *Config) Save(s Server, _ *struct{}) error {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	defer c.save()
-	ns, ok := c.Servers[s.ID]
-	if !ok {
-		return ErrNoServer
-	}
-	s.Path = ns.Path
-	s.status = ns.status
-	c.Servers[s.ID] = s
-	return nil
-}
-
-func (c *Config) MapList(_ struct{}, list *[]Map) error {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-	*list = make([]Map, 0, len(c.Maps))
-	for _, m := range c.Maps {
-		*list = append(*list, m)
-	}
-	return nil
-}
-
 func (c *Config) createServer(name, path string) int {
 	c.mu.Lock()
 	defer c.mu.Unlock()
