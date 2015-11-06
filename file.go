b5dde4f4e7ee61445bda952ece3b3f95ade1d72d
Michael Woolnough <michael.woolnough@gmail.com>
2015-11-06 15:21:45 +0000

    Rewrite start

--- file.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"net/http"
-
-	"github.com/MJKWoolnough/httpdir"
-)
-
-var dir http.FileSystem = httpdir.Default
--- file_debug.go
+++
@@ -1,9 +0,0 @@
-// +build debug
-
-package main
-
-import "net/http"
-
-func init() {
-	dir = http.Dir("")
-}
