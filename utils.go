04b0a830161b665f1d94c726334dd20cd7e806b0
Michael Woolnough <michael.woolnough@gmail.com>
2015-11-08 10:49:53 +0000

    Restored utility funcs

---
+++ utils.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"archive/zip"
+	"io"
+	"os"
+	"path"
+	"syscall"
+
+	"github.com/MJKWoolnough/byteio"
+)
+
+func writeError(w *byteio.StickyWriter, err error) {
+	w.WriteUint8(0)
+	writeString(w, err.Error())
+}
+
+func readString(r *byteio.StickyReader) string {
+	length := r.ReadUint16()
+	str := make([]byte, int(length))
+	io.ReadFull(r, str)
+	return string(str)
+}
+
+func writeString(w *byteio.StickyWriter, str string) {
+	w.WriteUint16(uint16(len(str)))
+	w.Write([]byte(str))
+}
+
+func moveFile(from, to string) error {
+	err := os.Rename(from, to)
+	if e, ok := err.(*os.LinkError); !ok || e.Err != syscall.EXDEV {
+		return err
+	}
+	fromf, err := os.Open(from)
+	if err != nil {
+		return err
+	}
+	defer fromf.Close()
+	tof, err := os.Create(to)
+	if err != nil {
+		return err
+	}
+	defer tof.Close()
+	_, err = io.Copy(tof, fromf)
+	return err
+}
+
+func unzip(zr *zip.Reader, dest string) error {
+	for _, f := range zr.File {
+		name := path.Join(dest, f.Name)
+		if f.FileInfo().IsDir() {
+			err := os.MkdirAll(name, f.Mode())
+			if err != nil {
+				return err
+			}
+			continue
+		}
+		inf, err := f.Open()
+		if err != nil {
+			return err
+		}
+		of, err := os.OpenFile(name, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			inf.Close()
+			return err
+		}
+		_, err = io.Copy(of, inf)
+		inf.Close()
+		of.Close()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
