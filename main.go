ef11de462bddd2c98bb3a852a7c0a96b06b6abb7
Michael Woolnough <michael.woolnough@gmail.com>
2015-11-08 11:14:21 +0000

    Changed websocket setup

--- main.go
+++ main.go
@@ -28,7 +28,8 @@ func main() {
 
 	rpc.RegisterName("RPC", RPC{c})
 
-	http.Handle("/transfer", websocket.Handler(handleFile))
+	t := Transfer{c}
+	http.Handle("/transfer", websocket.Handler(t.Websocket))
 	http.Handle("/rpc", websocket.Handler(func(conn *websocket.Conn) { jsonrpc.ServeConn(conn) }))
 	http.Handle("/", http.FileServer(dir))
 	l, err := net.Listen("tcp", c.ServerSettings.ListenAddr)
