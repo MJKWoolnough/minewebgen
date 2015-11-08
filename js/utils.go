d0d115176c0fae8aa7874e12708e1969067e3bdf
Michael Woolnough <michael.woolnough@gmail.com>
2015-11-08 14:15:05 +0000

    Added ReadError, WriteString and ReadString to utils

--- js/utils.go
+++ js/utils.go
@@ -1,9 +1,35 @@
 package main
 
-import "honnef.co/go/js/dom"
+import (
+	"errors"
+	"io"
+
+	"github.com/MJKWoolnough/byteio"
+	"honnef.co/go/js/dom"
+)
 
 func WrapEvent(f func(...dom.Element), c ...dom.Element) func(dom.Event) {
 	return func(dom.Event) {
 		go f(c...)
 	}
+}
+
+func ReadError(r *byteio.StickyReader) error {
+	s := readString(r)
+	if r.Err != nil {
+		return r.Err
+	}
+	return errors.New(s)
+}
+
+func WriteString(w *byteio.StickyWriter, s string) {
+	w.WriteUInt16(uint16(len(s)))
+	w.Write([]byte(s))
+}
+
+func ReadString(r *byteio.StickyReader) string {
+	length := r.ReadUint16()
+	str := make([]byte, int(length))
+	io.ReadFull(r, str)
+	return string(str)
 }
