6c00076dabbdadb7a46ae7255162dbf32d3776b4
Michael Woolnough <michael.woolnough@gmail.com>
2015-11-09 14:00:29 +0000

    Implemented simple map creation and SuperFlat creation

--- js/maps.go
+++ js/maps.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"strconv"
 	"time"
 
@@ -71,9 +72,153 @@ func mapsTab(c dom.Element) {
 	c.AppendChild(t)
 }
 
+var gameModes = [...]string{"Survival", "Creative", "Adventure", "Hardcore", "Spectator"}
+
 func createMap(o *overlay.Overlay) func(dom.Element) {
 	return func(c dom.Element) {
+		name := xform.InputText("name", "")
+		name.Required = true
+		gmOpts := make([]xform.Option, len(gameModes))
+		for i, m := range gameModes {
+			gmOpts[i] = xform.Option{
+				Label: m,
+				Value: strconv.Itoa(i),
+			}
+		}
+		gameMode := xform.SelectBox("gamemode", gmOpts...)
+		seed := xform.InputText("seed", "")
+		structures := xform.InputCheckbox("structures", true)
+		cheats := xform.InputCheckbox("cheats", false)
+		fs := xdom.Fieldset()
+		fs.AppendChild(xjs.SetInnerText(xdom.Legend(), "Create Map"))
+		c.AppendChild(xjs.AppendChildren(xdom.Form(), fs))
+		dataParser := func(mode int) func() (data.DefaultMap, error) {
+			return func() (data.DefaultMap, error) {
+				data := data.DefaultMap{
+					Mode: mode,
+				}
+				var err error
+				data.Name = name.Value
+				si := gameMode.SelectedIndex
+				if si < 0 || si >= len(gameModes) {
+					return data, errors.New("invalid gamemode")
+				}
+				if seed.Value == "" {
+					seed.Value = "0"
+				}
+				data.Seed, err = strconv.ParseInt(seed.Value, 10, 64)
+				if err != nil {
+					return data, err
+				}
+				data.Structures = structures.Checked
+				data.Cheats = cheats.Checked
+				return data, nil
+			}
+		}
+		xjs.AppendChildren(fs,
+			xform.Label("Name", "name"),
+			name,
+			xdom.Br(),
+			xform.Label("Game Mode", "gamemode"),
+			gameMode,
+			xdom.Br(),
+			xform.Label("Level Seed", "seed"),
+			seed,
+			xdom.Br(),
+			xform.Label("Structures", "structures"),
+			structures,
+			xdom.Br(),
+			xform.Label("Cheats", "cheats"),
+			cheats,
+			xdom.Br(),
+			tabs.New([]tabs.Tab{
+				{"Default", createMapMode(0, o, dataParser(0))},
+				{"Super Flat", createSuperFlatMap(o, dataParser(1))},
+				{"Large Biomes", createMapMode(2, o, dataParser(2))},
+				{"Amplified", createMapMode(3, o, dataParser(3))},
+				{"Customised", createCustomisedMap(o, dataParser(4))},
+			}),
+		)
+	}
+}
 
+var worldTypes = [...]string{
+	"The standard minecraft map generation.",
+	"A simple generator allowing customised levels of blocks.",
+	"The standard minecraft map generation, but tweaked to allow for much larger biomes.",
+	"The standard minecraft map generation, but tweaked to stretch the land upwards.",
+	"A completely customiseable generator.",
+}
+
+func createMapMode(mode int, o *overlay.Overlay, dataParser func() (data.DefaultMap, error)) func(dom.Element) {
+	submit := xform.InputSubmit("Create Map")
+	submit.AddEventListener("click", false, func(e dom.Event) {
+		data, err := dataParser()
+		if err != nil {
+			xjs.Alert("Error parsing values: %s", err)
+			return
+		}
+		e.PreventDefault()
+		go func() {
+			err = RPC.CreateDefaultMap(data)
+			if err != nil {
+				xjs.Alert("Error parsing values: %s", err)
+				return
+			}
+			o.Close()
+
+		}()
+	})
+	return func(c dom.Element) {
+		xjs.AppendChildren(c,
+			xjs.SetPreText(xdom.Div(), worldTypes[mode]),
+			xdom.Br(),
+			submit,
+		)
+	}
+}
+
+func createSuperFlatMap(o *overlay.Overlay, dataParser func() (data.DefaultMap, error)) func(dom.Element) {
+	// create better UI here
+	d := xdom.Div()
+	gs := xform.InputText("settings", "0")
+	submit := xform.InputSubmit("Create Map")
+	xjs.AppendChildren(d,
+		xjs.SetPreText(xdom.Div(), worldTypes[1]),
+		xform.Label("Generator Settings", "settings"),
+		gs,
+		xdom.Br(),
+		submit,
+	)
+	submit.AddEventListener("click", false, func(e dom.Event) {
+		d, err := dataParser()
+		if err != nil {
+			xjs.Alert("Error parsing values: %s", err)
+			return
+		}
+		e.PreventDefault()
+		go func() {
+			err = RPC.CreateSuperflatMap(data.SuperFlatMap{
+				DefaultMap:        d,
+				GeneratorSettings: gs.Value,
+			})
+			if err != nil {
+				xjs.Alert("Error parsing values: %s", err)
+				return
+			}
+			o.Close()
+
+		}()
+	})
+	return func(c dom.Element) {
+		c.AppendChild(d)
+	}
+}
+
+func createCustomisedMap(o *overlay.Overlay, dataParser func() (data.DefaultMap, error)) func(dom.Element) {
+	d := xdom.Div()
+	return func(c dom.Element) {
+		c.AppendChild(d)
 	}
 }
 
@@ -89,7 +234,7 @@ func mapGeneral(m data.Map) func(dom.Element) {
 			name.Required = true
 			opts := make([]xform.Option, 1, len(servers)+1)
 			opts[0] = xform.Option{
-				Label:    "-- None -- ",
+				Label:    "-- None --",
 				Value:    "-1",
 				Selected: m.Server == -1,
 			}
