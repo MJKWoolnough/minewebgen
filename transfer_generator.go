8c3a0ee661e333da4b4ea0834df61b276010b4ff
Michael Woolnough <michael.woolnough@gmail.com>
2015-11-23 12:14:31 +0000

    Implemeneted generator uploading/downloading

--- transfer_generator.go
+++ transfer_generator.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"io"
 	"os"
+	"path"
+	"strings"
 
 	"github.com/MJKWoolnough/byteio"
 )
 
-func (t Transfer) generator(name string, r *byteio.StickyReader, w *byteio.StickyWriter, f *os.File, size int64) error {
-	return nil
+func (t Transfer) generator(name string, _ *byteio.StickyReader, w *byteio.StickyWriter, f *os.File, _ int64) error {
+	name = strings.Replace(name, "/", "", -1)
+	err := t.c.Generators.LoadGenerator(name, f)
+	if err != nil {
+		return err
+	}
+	f.Seek(0, 0)
+	g, err := os.Create(path.Join(t.c.Settings().DirGenerators, name+".gen"))
+	if err != nil {
+		return err
+	}
+	defer g.Close()
+	_, err = io.Copy(g, f)
+	return err
 }
