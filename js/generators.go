a03ec53dd937f11ff934831cd31a47b3265bb764
Michael Woolnough <michael.woolnough@gmail.com>
2015-11-25 12:30:11 +0000

    generatorProfile saves the created DOM node, instead of just the data

--- js/generators.go
+++ js/generators.go
@@ -7,7 +7,6 @@ import (
 	"github.com/MJKWoolnough/gopherjs/tabs"
 	"github.com/MJKWoolnough/gopherjs/xdom"
 	"github.com/MJKWoolnough/gopherjs/xjs"
-	"github.com/MJKWoolnough/minewebgen/internal/data"
 	"honnef.co/go/js/dom"
 )
 
@@ -61,75 +60,71 @@ func GeneratorsTab(c dom.Element) {
 }
 
 func generatorProfile(name string) func(dom.Element) {
-	var (
-		loaded bool
-		g      data.Generator
-	)
+	var d dom.Node
 	return func(c dom.Element) {
-		if !loaded {
-			var err error
-			g, err = RPC.Generator(name)
+		if d == nil {
+			g, err := RPC.Generator(name)
 			if err != nil {
 				xjs.Alert("Error while getting generator settings: %s", err)
 				return
 			}
-			loaded = true
+			tTable := xjs.AppendChildren(xdom.Table(), xjs.AppendChildren(xdom.Thead(), xjs.AppendChildren(xdom.Tr(),
+				xjs.SetInnerText(xdom.Th(), "Colour"),
+				xjs.SetInnerText(xdom.Th(), "Colour Code"),
+				xjs.SetInnerText(xdom.Th(), "Name"),
+			)))
+			for _, t := range g.Terrain {
+				colour := xdom.Td()
+				cc := fmt.Sprintf("rgb(%d, %d, %d)", t.Colour.R, t.Colour.G, t.Colour.B)
+				colour.Style().SetProperty("background-color", cc, "")
+				colour.Style().SetProperty("border", "1px solid #000", "")
+				tTable.AppendChild(xjs.AppendChildren(xdom.Tr(),
+					colour,
+					xjs.SetInnerText(xdom.Td(), cc),
+					xjs.SetInnerText(xdom.Td(), t.Name),
+				))
+			}
+			bTable := xjs.AppendChildren(xdom.Table(), xjs.AppendChildren(xdom.Thead(), xjs.AppendChildren(xdom.Tr(),
+				xjs.SetInnerText(xdom.Th(), "Colour"),
+				xjs.SetInnerText(xdom.Th(), "Colour Code"),
+				xjs.SetInnerText(xdom.Th(), "Name"),
+			)))
+			for _, b := range g.Biomes {
+				colour := xdom.Td()
+				cc := fmt.Sprintf("rgb(%d, %d, %d)", b.Colour.R, b.Colour.G, b.Colour.B)
+				colour.Style().SetProperty("background-color", cc, "")
+				colour.Style().SetProperty("border", "1px solid #000", "")
+				bTable.AppendChild(xjs.AppendChildren(xdom.Tr(),
+					colour,
+					xjs.SetInnerText(xdom.Td(), cc),
+					xjs.SetInnerText(xdom.Td(), b.Name),
+				))
+			}
+			pTable := xjs.AppendChildren(xdom.Table(), xjs.AppendChildren(xdom.Thead(), xjs.AppendChildren(xdom.Tr(),
+				xjs.SetInnerText(xdom.Th(), "Colour"),
+				xjs.SetInnerText(xdom.Th(), "Colour Code"),
+				xjs.SetInnerText(xdom.Th(), "Name"),
+			)))
+			for _, p := range g.Plants {
+				colour := xdom.Td()
+				cc := fmt.Sprintf("rgb(%d, %d, %d)", p.Colour.R, p.Colour.G, p.Colour.B)
+				colour.Style().SetProperty("background-color", cc, "")
+				colour.Style().SetProperty("border", "1px solid #000", "")
+				pTable.AppendChild(xjs.AppendChildren(xdom.Tr(),
+					colour,
+					xjs.SetInnerText(xdom.Td(), cc),
+					xjs.SetInnerText(xdom.Td(), p.Name),
+				))
+			}
+			d = xjs.AppendChildren(xdom.Div(),
+				xjs.SetInnerText(xdom.H2(), "Terrain"),
+				tTable,
+				xjs.SetInnerText(xdom.H2(), "Biomes"),
+				bTable,
+				xjs.SetInnerText(xdom.H2(), "Plants"),
+				pTable,
+			)
 		}
-		tTable := xjs.AppendChildren(xdom.Table(), xjs.AppendChildren(xdom.Thead(), xjs.AppendChildren(xdom.Tr(),
-			xjs.SetInnerText(xdom.Th(), "Colour"),
-			xjs.SetInnerText(xdom.Th(), "Colour Code"),
-			xjs.SetInnerText(xdom.Th(), "Name"),
-		)))
-		for _, t := range g.Terrain {
-			colour := xdom.Td()
-			cc := fmt.Sprintf("rgb(%d, %d, %d)", t.Colour.R, t.Colour.G, t.Colour.B)
-			colour.Style().SetProperty("background-color", cc, "")
-			colour.Style().SetProperty("border", "1px solid #000", "")
-			tTable.AppendChild(xjs.AppendChildren(xdom.Tr(),
-				colour,
-				xjs.SetInnerText(xdom.Td(), cc),
-				xjs.SetInnerText(xdom.Td(), t.Name),
-			))
-		}
-		bTable := xjs.AppendChildren(xdom.Table(), xjs.AppendChildren(xdom.Thead(), xjs.AppendChildren(xdom.Tr(),
-			xjs.SetInnerText(xdom.Th(), "Colour"),
-			xjs.SetInnerText(xdom.Th(), "Colour Code"),
-			xjs.SetInnerText(xdom.Th(), "Name"),
-		)))
-		for _, b := range g.Biomes {
-			colour := xdom.Td()
-			cc := fmt.Sprintf("rgb(%d, %d, %d)", b.Colour.R, b.Colour.G, b.Colour.B)
-			colour.Style().SetProperty("background-color", cc, "")
-			colour.Style().SetProperty("border", "1px solid #000", "")
-			bTable.AppendChild(xjs.AppendChildren(xdom.Tr(),
-				colour,
-				xjs.SetInnerText(xdom.Td(), cc),
-				xjs.SetInnerText(xdom.Td(), b.Name),
-			))
-		}
-		pTable := xjs.AppendChildren(xdom.Table(), xjs.AppendChildren(xdom.Thead(), xjs.AppendChildren(xdom.Tr(),
-			xjs.SetInnerText(xdom.Th(), "Colour"),
-			xjs.SetInnerText(xdom.Th(), "Colour Code"),
-			xjs.SetInnerText(xdom.Th(), "Name"),
-		)))
-		for _, p := range g.Plants {
-			colour := xdom.Td()
-			cc := fmt.Sprintf("rgb(%d, %d, %d)", p.Colour.R, p.Colour.G, p.Colour.B)
-			colour.Style().SetProperty("background-color", cc, "")
-			colour.Style().SetProperty("border", "1px solid #000", "")
-			pTable.AppendChild(xjs.AppendChildren(xdom.Tr(),
-				colour,
-				xjs.SetInnerText(xdom.Td(), cc),
-				xjs.SetInnerText(xdom.Td(), p.Name),
-			))
-		}
-		xjs.AppendChildren(c,
-			xjs.SetInnerText(xdom.H2(), "Terrain"),
-			tTable,
-			xjs.SetInnerText(xdom.H2(), "Biomes"),
-			bTable,
-			xjs.SetInnerText(xdom.H2(), "Plants"),
-			pTable,
-		)
+		c.AppendChild(d)
 	}
 }
